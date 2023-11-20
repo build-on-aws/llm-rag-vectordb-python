9c9abe7d38554ee5d9a19cc89084083086b62222
Suman Debnath <56056673+debnsuma@users.noreply.github.com>
2023-11-20 10:32:50 -0500

    adding sql code and the notebook for the resume-building-app

--- resume-screening-app/sql_commands.sql
+++ resume-screening-app/sql_commands.sql
@@ -19,8 +19,21 @@ SELECT * FROM langchain_pg_embedding;
 SELECT langchain_pg_embedding.collection_id,
        langchain_pg_embedding.embedding,
        langchain_pg_embedding.document
-FROM langchain_pg_embedding;
+FROM langchain_pg_embedding
 
+-- SHOW all the collections (After the start of resume building app)
+SELECT * FROM langchain_pg_collection;
+
+--- SHOW all the embeddings (After the start of resume building app)
+SELECT langchain_pg_embedding.collection_id,
+       langchain_pg_embedding.embedding,
+       langchain_pg_embedding.document
+FROM langchain_pg_embedding
+WHERE langchain_pg_embedding.collection_id = (
+                                                SELECT uuid
+                                                FROM langchain_pg_collection
+                                                WHERE name = 'resume-embeddings-index'
+                                            );
 
 -- [CLEAN UP] Dropping the tables
 DROP TABLE langchain_pg_embedding;
